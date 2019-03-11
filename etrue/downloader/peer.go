4a8ed1bc14aca8bf60f977c80e3a30fe45c91474
jiyilanzhou <450595468@qq.com>
2019-03-11 18:02:45 +0800

    core etrue : add distinguish fetcher and downloader for head and body

--- etrue/downloader/peer.go
+++ etrue/downloader/peer.go
@@ -92,7 +92,7 @@ func (w *lightPeerWrapper) RequestReceipts([]common.Hash, bool) error {
 	panic("RequestReceipts not supported in light client mode sync")
 }
 
-func (w *lightPeerWrapper) RequestBodies([]common.Hash, bool, string) error {
+func (w *lightPeerWrapper) RequestBodies([]common.Hash, bool, uint64) error {
 	panic("RequestReceipts not supported in light client mode sync")
 }
 
